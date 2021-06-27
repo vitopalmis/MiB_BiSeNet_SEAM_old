4396ee95e49709b407fd3a9e0e8e2184d7b22e49
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2021-06-27 19:13:57 +0200

    Add BiSeNet and context_path

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,5 @@
 from .deeplab import DeeplabV3
 from .residual import IdentityResidualBlock, ResidualBlock
 from .misc import GlobalAvgPool2d
+from .build_BiSeNet import BiSeNet
+from .build_contextpath import build_contextpath
