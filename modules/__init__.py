6e608e5fb4cc51b8dbf8e33b66383e1d13339ffb
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2021-06-28 17:00:49 +0200

    Fabio suggestions 1

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,5 @@
 from .deeplab import DeeplabV3
 from .residual import IdentityResidualBlock, ResidualBlock
 from .misc import GlobalAvgPool2d
-from .build_BiSeNet import BiSeNet
+from .build_BiSeNet import *
 from .build_contextpath import build_contextpath
