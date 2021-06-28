f1f6fe1423219a3ca07188b397aad1181ffecd27
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2021-06-28 15:51:20 +0200

    Fabio suggestions 1

--- segmentation_module.py
+++ segmentation_module.py
@@ -78,34 +78,62 @@ class IncrementalSegmentationModule(nn.Module):
         # output dimension = number of classes for task c, c index = i
         # kernel = 1
         self.cls = nn.ModuleList(
-            [nn.Conv2d(c, c, 1) for c in classes]
+            [nn.Conv2d(32, c, 1) for c in classes]  # cambiato da 'c' a 32
         )
         self.classes = classes
         self.numClasses = numClasses
         self.tot_classes = reduce(lambda a, b: a + b, self.classes)
         self.means = None
+        
+        self.supervision1 = nn.Conv2d(in_channels=1024, out_channels=num_classes, kernel_size=1)
+        self.supervision2 = nn.Conv2d(in_channels=2048, out_channels=num_classes, kernel_size=1)
 
-    def _network(self, x, ret_intermediate=False):
+    def _network(self, x):
 
         # take the input, put into bisenet
-        x_b = self.bisenet(x)
+        x_b = self.bisenet(x)  # x_b contains probabilities
+        # x_f0, 1, 2 contain features which represents the pixel codifications
+        x_f0 = x_b[0]
+        x_f1 = x_b[1]  # cx1
+        x_f2 = x_b[2]  # cx2
         
-        out = []
+        cx1_sup = self.supervision1(x_f1)
+        cx2_sup = self.supervision2(x_f2)
+        cx1_sup = torch.nn.functional.interpolate(cx1_sup, size=input.size()[-2:], mode='bilinear')
+        cx2_sup = torch.nn.functional.interpolate(cx2_sup, size=input.size()[-2:], mode='bilinear')
+        
+        out_f0 = []
+        out_f1 = []
+        out_f2 = []
         
         # for each convolution in cls, add the result of the 
-        # convolution applied on the output of bisenet
+        # convolution applied on the output of FFM
         for mod in self.cls:
-            out.append(mod(x_b))
+            out_f0.append(mod(x_f0))
             
         # concatenates the conv results
         # (out is a list of tensors)
-        x_o = torch.cat(out, dim=1)
+        x_o_f0 = torch.cat(out_f0, dim=1)
+        
+        # for each convolution in cls, add the result of the 
+        # convolution applied on the output of cx1
+        for mod in self.cls:
+            out_f1.append(mod(cx1_sup))
+            
+        # concatenates the conv results
+        # (out is a list of tensors)
+        x_o_f1 = torch.cat(out_f1, dim=1)
+        
+        # for each convolution in cls, add the result of the 
+        # convolution applied on the output of cx2
+        for mod in self.cls:
+            out_f2.append(mod(cx2_sup))
+            
+        # concatenates the conv results
+        # (out is a list of tensors)
+        x_o_f2 = torch.cat(out_f2, dim=1)
 
-        # if you want the intermediate results:
-        if ret_intermediate:
-            return x_o, x_b
-        # else:
-        return x_o
+        return x_o_f0, x_o_f1, x_o_f2, x_b
 
     def init_new_classifier(self, device):
         # take the last conv layer (conv layer of t-1)
@@ -131,19 +159,23 @@ class IncrementalSegmentationModule(nn.Module):
     def forward(self, x, scales=None, do_flip=False, ret_intermediate=False):
         out_size = x.shape[-2:]
 
-        # returns x_o of the _network method (or x_o, x_b, x_pl if ret_intermediate = True)
-        out = self._network(x, ret_intermediate)
+        # returns the features maps x_o_f0, x_o_f1, x_o_f2 and the probabilities x_b (the final output of BiSeNet)
+        out = self._network(x)
 
         # take only x_o
-        sem_logits = out[0] if ret_intermediate else out
+        sem_logits_f0 = out[0]
+        sem_logits_f1 = out[1]
+        sem_logits_f2 = out[2]
 
         # up_sample x_o to the original dimension of the input
-        sem_logits = functional.interpolate(sem_logits, size=out_size, mode="bilinear", align_corners=False)
+        sem_logits_f0 = functional.interpolate(sem_logits_f0, size=out_size, mode="bilinear", align_corners=False)
+        sem_logits_f1 = functional.interpolate(sem_logits_f1, size=out_size, mode="bilinear", align_corners=False)
+        sem_logits_f2 = functional.interpolate(sem_logits_f2, size=out_size, mode="bilinear", align_corners=False)
 
         if ret_intermediate:
-            return sem_logits, {"bisenet": out[1]}
+            return sem_logits_f0, sem_logits_f1, sem_logits_f2, {"bisenet": out[3]}
 
-        return sem_logits, {}
+        return sem_logits_f0, sem_logits_f1, sem_logits_f2, {}
 
     # fix batch normalization during training:
     def fix_bn(self):
